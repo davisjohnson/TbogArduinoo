221578b38f0d4b4da33cba90ed129b040c25184d
davisjohnson <43835847+davisjohnson@users.noreply.github.com>
2019-11-23 12:09:10 -0800

    Add files via upload

---
+++ code/code.ino
@@ -0,0 +1,26 @@
+boolean countDown = false;
+boolean normal = false;
+boolean gameOver = false;
+boolean threePoint = false;
+int count = 0;
+const int analogInPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+  timeOn = millis();
+}
+
+void loop() {
+  if(countDown) {
+      
+  }
+  if(normal) {
+    
+  }
+  if(threePoint) {
+    
+  }
+  if(gameOver) {
+    
+  }
+}
