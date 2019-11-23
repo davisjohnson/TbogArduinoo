2ebe97ed2330bdd312468c5645443f49968cb657
davisjohnson <43835847+davisjohnson@users.noreply.github.com>
2019-11-23 12:26:24 -0800

    Add files via upload

--- code/code.ino
+++ code/code.ino
@@ -3,22 +3,37 @@ boolean normal = false;
 boolean gameOver = false;
 boolean threePoint = false;
 int count = 0;
-const int analogInPin = A0;
+const int digitalInPin = D7;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(digitalInPin, INPUT);
   timeOn = millis();
 }
 
 void loop() {
+  currentTime = 1000*(millis()-timeOn);
   if(countDown) {
-      
+      //soundEffect
+      if(currentTime>=5) {
+        count = 0;
+        countDown = false;
+        normal true;
+      }
   }
   if(normal) {
     
   }
   if(threePoint) {
-    
+   if(digitalRead(digitalInPin) == HIGH) {
+    count+=3;
+    //soundEffect
+   }
+   if(currentTime >= 65) {
+    threePoint = false;
+    gameOver = true;
+    //soundEffect
+   }
   }
   if(gameOver) {
     
